df418ef632a2666109931523b0e299df07c42ddd
Brennan Borlaug <brennan.borlaug@ischool.berkeley.edu>
2016-04-28 00:08:05 -0600

    Update load_data.sql

--- data/load_data.sql
+++ data/load_data.sql
@@ -1,2 +1,2 @@
-COPY data.results FROM '/data/MIDS_205_elections/data/election_results.csv' CSV HEADER; -- Switch to your directory
-COPY data.google FROM '/data/MIDS_205_elections/data/google_traffic.csv' CSV HEADER; -- Switch to your directory
+COPY data.results FROM '/data/MIDS_205_elections/data/election_results.csv' CSV HEADER; -- Switch to your path
+COPY data.google FROM '/data/MIDS_205_elections/data/google_traffic.csv' CSV HEADER; -- Switch to your path
